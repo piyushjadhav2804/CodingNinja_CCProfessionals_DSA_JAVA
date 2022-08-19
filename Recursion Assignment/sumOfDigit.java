691f6df0581c1805d512893d4965362fee443b2f
piyushjadhav2804 <71430828+piyushjadhav2804@users.noreply.github.com>
2022-08-19 14:01:14 +0530

    Create sumOfDigit.java

---
+++ Recursion Assignment/sumOfDigit.java
@@ -0,0 +1,14 @@
+
+public class solution {
+
+    static int count=0;
+    
+	public static int sumOfDigits(int input){
+		// Write your code here
+        
+        if(input == 0)
+            return 0;
+                
+		return input%10 + sumOfDigits(input/10);
+	}
+}
